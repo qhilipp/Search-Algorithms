aa5f8031dd0fbc4625c5ac50a17d94bb55136aa1
Philipp Kathöfer <philippkathoefr@gmail.com>
2024-11-17 19:10:09 -0800

    Setup example code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-import DataStructures.Cartesian;
-import DataStructures.Vector;
+import searchAlgorithms.GeneralSearch;
+import searchStrategie.*;
+import stateSpace.Cartesian;
+import util.Vector;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Cartesian space = new Cartesian(1, new Vector(3, 4));
+		Cartesian space = new Cartesian(2, new Vector(3, 4));
+		GeneralSearch<Vector> search = new GeneralSearch(space, Queue.class);
+		for(Vector v : search.search()) {
+			System.out.println(v + " ");
+		}
 	}
 
 }
