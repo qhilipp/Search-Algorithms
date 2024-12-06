d1973efecca22f6b4d7f406741a23fc3de33e14d
Philipp Kathöfer <philippkathoefr@gmail.com>
2024-12-05 22:04:00 -0800

    Make UniformSearch extend AStarSearch

--- src/searchAlgorithms/UniformSearch.java
+++ src/searchAlgorithms/UniformSearch.java
@@ -1,14 +1,12 @@
 package searchAlgorithms;
 
 import heuristic.ConstantHeuristic;
-import pathEvaluation.SumPathEvaluator;
-import searchStrategy.Uniform;
 import stateSpace.StateSpace;
 
-public class UniformSearch<Node> extends GeneralSearch<Node> {
+public class UniformSearch<Node> extends AStarSearch<Node> {
 
 	public UniformSearch(StateSpace<Node> space) {
-		super(space, new Uniform<>(), new SumPathEvaluator<>(), new ConstantHeuristic<>());
+		super(space, new ConstantHeuristic<>());
 	}
 
 }
