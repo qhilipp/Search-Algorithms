a183a17e59f31aa8f2d0506d3efbbf98eb99ee00
Philipp Kathöfer <philippkathoefr@gmail.com>
2024-12-23 14:54:34 -0800

    Fix loop handling

--- src/loopHandling/LoopHandler.java
+++ src/loopHandling/LoopHandler.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 
 public interface LoopHandler<Node> {
 
-	public void initialize();
 	public boolean shouldVisitNode(Node node, ArrayList<Node> path, double pathCost, HashMap<Node, Double> minCostToNode);
 	
 }
--- src/loopHandling/TreeLoopHandler.java
+++ src/loopHandling/TreeLoopHandler.java
@@ -6,9 +6,6 @@ import java.util.HashMap;
 public class TreeLoopHandler<Node> implements LoopHandler<Node> {
 
 	@Override
-	public void initialize() {}
-
-	@Override
 	public boolean shouldVisitNode(Node node, ArrayList<Node> path, double pathCost, HashMap<Node, Double> minCostToNode) {
 		return true;
 	}
