d41ba0c5c2be58cd85b93bda7ede2b3579490c8a
Philipp Kathöfer <philippkathoefr@gmail.com>
2024-12-24 14:43:32 -0800

    Generalize Rectangle constructor for n dimensions

--- src/util/Rectangle.java
+++ src/util/Rectangle.java
@@ -4,9 +4,15 @@ public class Rectangle {
 
 	public Vector position, size;
 	
-	public Rectangle(double x, double y, double width, double height) {
-		this.position = new Vector(x, y);
-		this.size = new Vector(width, height);
+	public Rectangle(double...components) {
+		double[] positionComponents = new double[components.length / 2];
+		double[] sizeComponents = new double[components.length / 2];
+		for(int i = 0; i < components.length / 2; i++) {
+			positionComponents[i] = components[i];
+			sizeComponents[i] = components[sizeComponents.length + i];
+		}
+		this.position = new Vector(positionComponents);
+		this.size = new Vector(sizeComponents);
 	}
 	
 	public double x() {
