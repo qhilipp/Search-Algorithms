d8b6e8c81d4911803b8706ded4d30e0c641e1b43
Philipp Kathöfer <philippkathoefr@gmail.com>
2025-01-03 11:44:59 -0800

    Add documentation to goalTest

--- src/goalTest/GoalTester.java
+++ src/goalTest/GoalTester.java
@@ -1,8 +1,17 @@
 package goalTest;
 
+/**
+ * tells the StateSpace whether a given Node is a goal or not
+ * @param 	<Node> 	the Node type used by the StateSpace
+ */
 @FunctionalInterface
 public interface GoalTester<Node> {
 
+	/**
+	 * 
+	 * @param 	node 	the node that you want to test
+	 * @return			whether the node is a goal or not
+	 */
 	public boolean isGoal(Node node);
 	
 }
--- src/goalTest/ListGoalTester.java
+++ src/goalTest/ListGoalTester.java
@@ -3,26 +3,50 @@ package goalTest;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * a GoalTester that specifies the goals with a list of goal nodes
+ * @param <Node>
+ */
 public class ListGoalTester<Node> implements GoalTester<Node> {
 
 	private ArrayList<Node> goals;
 	
+	/**
+	 * all given nodes are goals, all other are not
+	 * @param 	goals	the nodes that are goals
+	 */
 	public ListGoalTester(Node...goals) {
 		this.goals = new ArrayList<>(Arrays.asList(goals));
 	}
 	
+	/**
+	 * all given nodes are goals, all other are not
+	 * @param 	goals	the nodes that are goals
+	 */
 	public ListGoalTester(ArrayList<Node> goals) {
 		this.goals = goals;
 	}
 	
+	/**
+	 * add a node to the list of goals
+	 * @param 	goal	the node that is being added to the goals 
+	 */
 	public void add(Node goal) {
 		goals.add(goal);
 	}
 	
+	/**
+	 * remove a node from the list of goals
+	 * @param 	goal	the node that is being removed from the goals
+	 */
 	public void remove(Node goal) {
 		goals.remove(goal);
 	}
 	
+	/**
+	 * return all goal nodes
+	 * @return	the goal nodes
+	 */
 	public ArrayList<Node> getGoals() {
 		return goals;
 	}
